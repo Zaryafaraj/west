5ec8b2522cd37807f36b73f30129571c34a49220
Fathalian <fatalian@uw.edu>
2015-01-19 14:29:06 -0800

    show individual apartments

--- app/scripts/controllers/city.js
+++ app/scripts/controllers/city.js
@@ -7,6 +7,7 @@ angular.module('westApp')
   .controller('CityCtrl', ['$scope', '$routeParams', 'allApartments',
     function ($scope, $routeParams, allApartments) {
 
+      $scope.cityName = $routeParams.cityName;
       $scope.allApartments = allApartments;
 
     }]);
